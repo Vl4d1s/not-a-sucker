75d76525473ec813723886b410b37e99f36f5683
Vladis <vladismarkin@gmail.com>
2021-03-18 22:35:48 +0200

    uploading the initial files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("I am a backend server");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err.message}`);
+  } else {
+    console.log(`server is runnig on port ${port}`);
+  }
+});

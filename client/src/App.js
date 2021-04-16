91858c5bf97b1bf57e75cdba668a066f6cb71bf1
Vl4d1s <vladismarkin@gmail.com>
2021-04-16 16:31:56 +0300

    adding simple routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routs from "./components/routs/Routs";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <main>
+      <Router>
+        <Routs />
+      </Router>
+    </main>
+  );
 }
 
 export default App;
---
+++ client/src/components/routs/Routs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Search from "../pages/Search";
+
+const Routs = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Search} />
+    </Switch>
+  );
+};
+
+export default Routs;

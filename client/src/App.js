2ad805a974a22b5ac6f95ec31fc3f62bf47c8bd1
Vl4d1s <vladismarkin@gmail.com>
2021-04-27 18:39:52 +0300

    creating the search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import "./App.css";
+import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import HeaderMenu from "./components/HeaderMenu/HeaderMenu";
 import Routs from "./components/routs/Routs";
+import "./App.css";
 
-function App() {
-  return (
-    <main>
-      <Router>
-        <Routs />
-      </Router>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Router>
+          <HeaderMenu items={[["Login", "/"]]} />
+          <Routs />
+        </Router>
+      </main>
+    );
+  }
 }
 
 export default App;

b140735175bc89cfc083d9c2cdf8bb0814d51314
Vl4d1s <vladismarkin@gmail.com>
2021-05-10 14:18:45 +0300

    navigate to the main page on logo click

--- client/src/components/pages/Results/Results.js
+++ client/src/components/pages/Results/Results.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Image, Input } from "semantic-ui-react";
 import secoundLogo from "../../../assets/images/secoundLogo.png";
+import { Link } from "react-router-dom";
 
 const Results = () => {
   return (
     <div style={{ marginTop: "-50px", marginLeft: "150px" }}>
       <Image
+        as={Link}
+        to="/"
         src={secoundLogo}
         inline
         style={{ marginRight: "40px" }}
